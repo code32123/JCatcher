486ecac0ec38f83471b072c7fe5785909103f34a
Code32123 <jimmy.l.smythe@gmail.com>
2024-03-24 15:37:58 -0700

    Restructure to V2 format

--- source/main.cpp
+++ source/main.cpp
@@ -13,15 +13,22 @@
 
 #include "rapidjson/stringbuffer.h"
 #include <iostream>
+#include <sys/stat.h>
 
 using namespace rapidjson;
 
-Result http_download(const char *url)
+struct fileBuf {
+	u8 *buf = 0;
+	u32 size = 0;
+	s32 error = 0;
+};
+
+fileBuf http_download(const char *url)
 {
+	fileBuf retBuf;
 	Result ret=0;
 	httpcContext context;
 	char *newurl=NULL;
-	u8* framebuf_top;
 	u32 statuscode=0;
 	u32 contentsize=0, readsize=0, size=0;
 	u8 *buf, *lastbuf;
@@ -55,7 +62,8 @@ Result http_download(const char *url)
 			httpcCloseContext(&context);
 			if(newurl!=NULL) free(newurl);
 			printf("%d: err: 0" "\n", __LINE__);
-			return ret;
+			retBuf.error = ret;
+			return retBuf;
 		}
 
 		ret = httpcGetResponseStatusCode(&context, &statuscode);
@@ -63,14 +71,16 @@ Result http_download(const char *url)
 			httpcCloseContext(&context);
 			if(newurl!=NULL) free(newurl);
 			printf("%d: err: 1" "\n", __LINE__);
-			return ret;
+			retBuf.error = ret;
+			return retBuf;
 		}
 
 		if ((statuscode >= 301 && statuscode <= 303) || (statuscode >= 307 && statuscode <= 308)) {
 			if(newurl==NULL) newurl = (char*)malloc(0x1000); // One 4K page for new URL
 			if (newurl==NULL){
 				httpcCloseContext(&context);
-				return -1;
+				retBuf.error = -1;
+				return retBuf;
 			}
 			ret = httpcGetResponseHeader(&context, "Location", newurl, 0x1000);
 			url = newurl; // Change pointer to the url that we just learned
@@ -83,7 +93,8 @@ Result http_download(const char *url)
 		printf("%d: URL returned status: %" PRId32 "\n", __LINE__, statuscode);
 		httpcCloseContext(&context);
 		if(newurl!=NULL) free(newurl);
-		return -2;
+		retBuf.error = -2;
+		return retBuf;
 	}
 
 	// This relies on an optional Content-Length header and may be 0
@@ -92,7 +103,8 @@ Result http_download(const char *url)
 		httpcCloseContext(&context);
 		if(newurl!=NULL) free(newurl);
 		printf("%d: err: 2" "\n", __LINE__);
-		return ret;
+		retBuf.error = ret;
+		return retBuf;
 	}
 
 	//printf("%d: reported size: %" PRId32 "\n",__LINE__,contentsize);
@@ -102,7 +114,8 @@ Result http_download(const char *url)
 	if(buf==NULL){
 		httpcCloseContext(&context);
 		if(newurl!=NULL) free(newurl);
-		return -1;
+		retBuf.error = -1;
+		return retBuf;
 	}
 
 	do {
@@ -116,7 +129,8 @@ Result http_download(const char *url)
 					httpcCloseContext(&context);
 					free(lastbuf);
 					if(newurl!=NULL) free(newurl);
-					return -1;
+					retBuf.error = -1;
+					return retBuf;
 				}
 			}
 	} while (ret == (s32)HTTPC_RESULTCODE_DOWNLOADPENDING);	
@@ -125,7 +139,8 @@ Result http_download(const char *url)
 		httpcCloseContext(&context);
 		if(newurl!=NULL) free(newurl);
 		free(buf);
-		return -1;
+		retBuf.error = -1;
+		return retBuf;
 	}
 
 	// Resize the buffer back down to our actual final size
@@ -136,127 +151,152 @@ Result http_download(const char *url)
 		httpcCloseContext(&context);
 		free(lastbuf);
 		if (newurl != NULL) free(newurl);
-		return -1;
+		retBuf.error = -1;
+		return retBuf;
 	}
 	memset(buf + size, 0, 1); // Pad with 0
 
 	printf("%d: downloaded size: %" PRId32 "\n",__LINE__,size);
 
-	// DO STUFF WITH DOWNLOADED FILE HERE (buf, size)
-	FILE* file = fopen("downloadOut1.json", "w");
-	if (file == NULL) return -1;
+	retBuf.buf = buf;
+	retBuf.size = size;
+	retBuf.error = 0;
+
+	httpcCloseContext(&context);
+	// free(buf);
+	if (newurl!=NULL) free(newurl);
+	return retBuf;
+}
+
+void printParseError(ParseErrorCode parseError) {
+	switch (parseError) {
+	case kParseErrorDocumentEmpty:
+		printf("%d: Parser error kParseErrorDocumentEmpty\n", __LINE__);
+		break;
+	case kParseErrorDocumentRootNotSingular:
+		printf("%d: Parser error kParseErrorDocumentRootNotSingular\n", __LINE__);
+		break;
+	case kParseErrorValueInvalid:
+		printf("%d: Parser error kParseErrorValueInvalid\n", __LINE__);
+		break;
+	case kParseErrorObjectMissName:
+		printf("%d: Parser error kParseErrorObjectMissName\n", __LINE__);
+		break;
+	case kParseErrorObjectMissColon:
+		printf("%d: Parser error kParseErrorObjectMissColon\n", __LINE__);
+		break;
+	case kParseErrorObjectMissCommaOrCurlyBracket:
+		printf("%d: Parser error kParseErrorObjectMissCommaOrCurlyBracket\n", __LINE__);
+		break;
+	case kParseErrorArrayMissCommaOrSquareBracket:
+		printf("%d: Parser error kParseErrorArrayMissCommaOrSquareBracket\n", __LINE__);
+		break;
+	case kParseErrorStringUnicodeEscapeInvalidHex:
+		printf("%d: Parser error kParseErrorStringUnicodeEscapeInvalidHex \n", __LINE__);
+		break;
+	case kParseErrorStringUnicodeSurrogateInvalid:
+		printf("%d: Parser error kParseErrorStringUnicodeSurrogateInvalid \n", __LINE__);
+		break;
+	case kParseErrorStringEscapeInvalid:
+		printf("%d: Parser error kParseErrorStringEscapeInvalid  \n", __LINE__);
+		break;
+	case kParseErrorStringMissQuotationMark:
+		printf("%d: Parser error kParseErrorStringMissQuotationMark  \n", __LINE__);
+		break;
+	case kParseErrorStringInvalidEncoding:
+		printf("%d: Parser error kParseErrorStringInvalidEncoding  \n", __LINE__);
+		break;
+	case kParseErrorNumberTooBig:
+		printf("%d: Parser error kParseErrorNumberTooBig  \n", __LINE__);
+		break;
+	case kParseErrorNumberMissFraction:
+		printf("%d: Parser error kParseErrorNumberMissFraction  \n", __LINE__);
+		break;
+	case kParseErrorNumberMissExponent:
+		printf("%d: Parser error kParseErrorNumberMissExponent  \n", __LINE__);
+		break;
+	case kParseErrorTermination:
+		printf("%d: Parser error kParseErrorTermination  \n", __LINE__);
+		break;
+	case kParseErrorUnspecificSyntaxError:
+		printf("%d: Parser error kParseErrorUnspecificSyntaxError  \n", __LINE__);
+		break;
+	default:
+		printf("%d: Parser error fell throught to OTHER\n", __LINE__);
+	}
+}
+
+void showFileBuf(fileBuf fileBuf) {
+	u32 size = fileBuf.size;
+	if(size>(240*400*3*2))size = 240*400*3*2;
+
+	u8* framebuf_bottom;
+	framebuf_bottom = gfxGetFramebuffer(GFX_BOTTOM, GFX_LEFT, NULL, NULL);
+	memcpy(framebuf_bottom, fileBuf.buf, size);
+
+	gfxFlushBuffers();
+	gfxSwapBuffers();
+
+	framebuf_bottom = gfxGetFramebuffer(GFX_BOTTOM, GFX_LEFT, NULL, NULL);
+	memcpy(framebuf_bottom, fileBuf.buf, size);
+
+	gfxFlushBuffers();
+	gfxSwapBuffers();
+	gspWaitForVBlank();
+}
+
+void saveFileBuf(fileBuf fileBuf, const char* fileName) {
+	struct stat st = {0};
+
+	if (stat("JCatch", &st) == -1) {
+		mkdir("JCatch", 0777);
+	}
+	std::string filePath = "JCatch/";
+	filePath.append(fileName);
+	FILE* file = fopen(filePath.c_str(), "w");
+	if (file == NULL) {
+		return;
+		// return -1;
+	}
 	fseek(file, 0, SEEK_SET);
-	fwrite(buf, 1, size, file);
+	fwrite(fileBuf.buf, 1, fileBuf.size, file);
 	fclose(file);
+}
 
-	// JSON Parsing?
-
+void parseFileBuf(fileBuf fileBuf) {
 	Document document;
-	document.ParseInsitu((char*)buf);
+	document.ParseInsitu((char*)fileBuf.buf);
 	if (document.HasParseError()) {
-		switch (document.GetParseError()) {
-		case kParseErrorDocumentEmpty:
-			printf("%d: Parser error kParseErrorDocumentEmpty\n", __LINE__);
-			break;
-		case kParseErrorDocumentRootNotSingular:
-			printf("%d: Parser error kParseErrorDocumentRootNotSingular\n", __LINE__);
-			break;
-		case kParseErrorValueInvalid:
-			printf("%d: Parser error kParseErrorValueInvalid\n", __LINE__);
-			break;
-		case kParseErrorObjectMissName:
-			printf("%d: Parser error kParseErrorObjectMissName\n", __LINE__);
-			break;
-		case kParseErrorObjectMissColon:
-			printf("%d: Parser error kParseErrorObjectMissColon\n", __LINE__);
-			break;
-		case kParseErrorObjectMissCommaOrCurlyBracket:
-			printf("%d: Parser error kParseErrorObjectMissCommaOrCurlyBracket\n", __LINE__);
-			break;
-		case kParseErrorArrayMissCommaOrSquareBracket:
-			printf("%d: Parser error kParseErrorArrayMissCommaOrSquareBracket\n", __LINE__);
-			break;
-		case kParseErrorStringUnicodeEscapeInvalidHex:
-			printf("%d: Parser error kParseErrorStringUnicodeEscapeInvalidHex \n", __LINE__);
-			break;
-		case kParseErrorStringUnicodeSurrogateInvalid:
-			printf("%d: Parser error kParseErrorStringUnicodeSurrogateInvalid \n", __LINE__);
-			break;
-		case kParseErrorStringEscapeInvalid:
-			printf("%d: Parser error kParseErrorStringEscapeInvalid  \n", __LINE__);
-			break;
-		case kParseErrorStringMissQuotationMark:
-			printf("%d: Parser error kParseErrorStringMissQuotationMark  \n", __LINE__);
-			break;
-		case kParseErrorStringInvalidEncoding:
-			printf("%d: Parser error kParseErrorStringInvalidEncoding  \n", __LINE__);
-			break;
-		case kParseErrorNumberTooBig:
-			printf("%d: Parser error kParseErrorNumberTooBig  \n", __LINE__);
-			break;
-		case kParseErrorNumberMissFraction:
-			printf("%d: Parser error kParseErrorNumberMissFraction  \n", __LINE__);
-			break;
-		case kParseErrorNumberMissExponent:
-			printf("%d: Parser error kParseErrorNumberMissExponent  \n", __LINE__);
-			break;
-		case kParseErrorTermination:
-			printf("%d: Parser error kParseErrorTermination  \n", __LINE__);
-			break;
-		case kParseErrorUnspecificSyntaxError:
-			printf("%d: Parser error kParseErrorUnspecificSyntaxError  \n", __LINE__);
-			break;
-		default:
-			printf("%d: Parser error fell throught to OTHER\n", __LINE__);
-		}
-		return -2;
+		printParseError(document.GetParseError());
+		return;
+		// return -2;
 	}
 
 	printf("\nParsing to document succeeded.\n");
 	printf("\nAccess values in document:\n");
 	assert(document.IsObject());    // Document is a JSON value represents the root of DOM. Root can be either an object or array.
-
 	assert(document.HasMember("title"));
 	assert(document["title"].IsString());
 	printf("title = %s\n", document["title"].GetString());
-	// DONE
-
-	if(size>(240*400*3*2))size = 240*400*3*2;
-
-	framebuf_top = gfxGetFramebuffer(GFX_TOP, GFX_LEFT, NULL, NULL);
-	memcpy(framebuf_top, buf, size);
-
-	gfxFlushBuffers();
-	gfxSwapBuffers();
-
-	framebuf_top = gfxGetFramebuffer(GFX_TOP, GFX_LEFT, NULL, NULL);
-	memcpy(framebuf_top, buf, size);
-
-	gfxFlushBuffers();
-	gfxSwapBuffers();
-	gspWaitForVBlank();
-
-	httpcCloseContext(&context);
-	free(buf);
-	if (newurl!=NULL) free(newurl);
-
-	return 0;
+	assert(document.HasMember("description"));
+	assert(document["description"].IsString());
+	printf("description = %s\n", document["description"].GetString());
 }
 
 int main()
 {
-	Result ret=0;
+	fileBuf downloadedFile;
 	gfxInitDefault();
 	httpcInit(0); // Buffer size when POST/PUT.
 
-	consoleInit(GFX_BOTTOM,NULL);
+	consoleInit(GFX_TOP,NULL);
 
-	// ret=http_download("http://devkitpro.org/misc/httpexample_rawimg.rgb");
 	//ret = http_download("https://www.toptal.com/developers/feed2json/convert?url=http://feeds.feedburner.com/OffbeatOregonHistory");
-	//ret = http_download("https://www.toptal.com/developers/feed2json/convert?url=http://feeds.feedburner.com/OffbeatOregonHistory");
-	ret = http_download("http://jimmytech.net/RSS2JSON/?url=http://feeds.feedburner.com/OffbeatOregonHistory");
-
-	printf("%d: return from http_download: %" PRId32 "\n",__LINE__,ret);
+	downloadedFile = http_download("http://jimmytech.net/RSS2JSON/?url=http://feeds.feedburner.com/OffbeatOregonHistory");
+	printf("%d: error from http_download: %" PRId32 "\n",__LINE__,downloadedFile.error);
+	showFileBuf(downloadedFile);
+	saveFileBuf(downloadedFile, "cast.json");
+	parseFileBuf(downloadedFile);
 
 	// Main loop
 	while (aptMainLoop())
